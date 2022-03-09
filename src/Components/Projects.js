8436ffd4315459082e620f82c483b78e557e49d8
michaelbryanstark <84143645+michaelbryanstark@users.noreply.github.com>
2022-03-09 12:01:54 -0500

    cards are all even, links to projects working

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -12,38 +12,44 @@ function Projects() {
                 <div className="card-header">
                     <div className="row row-cols-1 row-cols-md-3 g-4">
                     <div className="col">
-                        <div className="card h-100">
-                        <img src="https://live.staticflickr.com/65535/51900488487_1b68a201c4_w.jpg" alt="new"/>
+                        <div className="card text-center h-100">
+                        <div className="image">
+                            <img src="https://live.staticflickr.com/65535/51900488487_1b68a201c4_w.jpg" alt="new" href="https://bdsqs.net" target='_blank' rel="noreferrer"/>
+                        </div>
                             <div className="card-body">
                                 <h3 className="card-title">BDS Quality Services</h3>
                                 <p className="card-text">Built using the Django framework and Python. BDS Quality Services is a company that provides quality assurance training and compliance.</p>
                             </div>
                             <div className="card-footer">
-                                <small className="text-muted">Copyright BDSQS</small>
+                                <a href="https://bdsqs.net" target='_blank' rel="noreferrer" className="text-muted">Visit BDS Quality Services!</a>
                             </div>
                         </div>
                     </div>
                     <div className="col">
-                        <div className="card h-100">
-                        <img src="https://media.istockphoto.com/photos/programmer-working-with-program-code-picture-id1075599562?k=20&m=1075599562&s=612x612&w=0&h=cDFY2kKyhFzSNNlDQsaxoekIW0v7iyaMBkxp11Fz33U=" alt="new"/>
+                        <div className="card text-center h-100">
+                            <div className="image">
+                                <img className="share" src="https://media.istockphoto.com/photos/programmer-working-with-program-code-picture-id1075599562?k=20&m=1075599562&s=612x612&w=0&h=cDFY2kKyhFzSNNlDQsaxoekIW0v7iyaMBkxp11Fz33U=" alt="new" href="www.google.com"/>
+                            </div>
                             <div className="card-body">
                                 <h3 className="card-title">CodeShare</h3>
-                                <p className="card-text">Social media app built for developers to share code/memes. Full stack React app with full CRUD built using JavaScript, Node.js, CSS, MongoDB/Mongoose, JWT-based Authentication.</p>
+                                <p className="card-text">Full stack React app built for developers to share code. Features full CRUD built using JavaScript, Node.js, CSS, MongoDB/Mongoose, JWT-based Authentication.</p>
                             </div>
                             <div className="card-footer">
-                                <small className="text-muted">Copyright Michael Stark</small>
+                            <a href="https://codeshare-app3.herokuapp.com/login" target='_blank' rel="noreferrer" className="text-muted">Visit CodeShare!</a>
                             </div>
                         </div>
                     </div>
                     <div className="col">
-                        <div className="card h-100">
-                        <img src="http://i.imgur.com/ALIGMnI.png" alt="new"/>
+                        <div className="card text-center h-100">
+                            <div className="image">
+                                <img className="dan" src="http://i.imgur.com/ALIGMnI.png" alt="new"/>
+                            </div>
                             <div className="card-body">
                                 <h3 className="card-title">The Penalty Box</h3>
                                 <p className="card-text">Personal Blog with full CRUD built using the Django Framework and Python. Users can sign up leave comments on blog posts.</p>
                             </div>
                             <div className="card-footer">
-                                <small className="text-muted">Copyright Michael Stark</small>
+                            <a href="https://penalty-box.herokuapp.com/" target='_blank' rel="noreferrer" className="text-muted">Visit The Penalty Box!</a>
                             </div>
                         </div>
                     </div>
