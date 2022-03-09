eebace7c90a4a9cb7d3509544e8fa1fd1ac94484
michaelbryanstark <84143645+michaelbryanstark@users.noreply.github.com>
2022-03-09 11:08:42 -0500

    projects page done pending further updates

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -25,25 +25,25 @@ function Projects() {
                     </div>
                     <div className="col">
                         <div className="card h-100">
-                            {/* <img src="..." className="card-img-top" alt="..."> */}
+                        <img src="https://media.istockphoto.com/photos/programmer-working-with-program-code-picture-id1075599562?k=20&m=1075599562&s=612x612&w=0&h=cDFY2kKyhFzSNNlDQsaxoekIW0v7iyaMBkxp11Fz33U=" alt="new"/>
                             <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                                <h5 className="card-title">CodeShare</h5>
+                                <p className="card-text">Social media app built for developers to share code/memes. Full stack React app with full CRUD built using JavaScript, Node.js, CSS, MongoDB/Mongoose, JWT-based Authentication.</p>
                             </div>
                             <div className="card-footer">
-                                <small className="text-muted">Last updated 3 mins ago</small>
+                                <small className="text-muted">Copyright Michael Stark</small>
                             </div>
                         </div>
                     </div>
                     <div className="col">
                         <div className="card h-100">
-                            {/* <img src="..." className="card-img-top" alt="..."> */}
+                        <img src="http://i.imgur.com/ALIGMnI.png" alt="new"/>
                             <div className="card-body">
-                                <h5 className="card-title">Card title</h5>
-                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                                <h5 className="card-title">The Penalty Box</h5>
+                                <p className="card-text">Personal Blog with full CRUD built using the Django Framework and Python. Users can sign up leave comments on blog posts.</p>
                             </div>
                             <div className="card-footer">
-                                <small className="text-muted">Last updated 3 mins ago</small>
+                                <small className="text-muted">Copyright Michael Stark</small>
                             </div>
                         </div>
                     </div>
