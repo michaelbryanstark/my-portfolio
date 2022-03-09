2c2a7eb122e8beae4fd472a0bd35e826f406c87f
michaelbryanstark <84143645+michaelbryanstark@users.noreply.github.com>
2022-03-09 10:46:08 -0500

    added image and infor for first project

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,57 +1,55 @@
 import React from "react";
 
+
 function Projects() {
     return(
         <div>
-            <div className="App-header">
-                <h1 className="welcome-text">
-                     Projects
-                </h1>
+                <div className="App-header">
+                    <h1 className="welcome-text">
+                         Projects
+                    </h1>
                 </div>
                 <div className="card-header">
-                <div className="row row-cols-1 row-cols-md-3 g-4">
-  <div className="col">
-    <div className="card h-100">
-      {/* <img src="..." className="card-img-top" alt="..."> */}
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      </div>
-      <div className="card-footer">
-        <small className="text-muted">Last updated 3 mins ago</small>
-      </div>
-    </div>
-  </div>
-  <div className="col">
-    <div className="card h-100">
-      {/* <img src="..." className="card-img-top" alt="..."> */}
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-      </div>
-      <div className="card-footer">
-        <small className="text-muted">Last updated 3 mins ago</small>
-      </div>
-    </div>
-  </div>
-  <div className="col">
-    <div className="card h-100">
-      {/* <img src="..." className="card-img-top" alt="..."> */}
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-      </div>
-      <div className="card-footer">
-        <small className="text-muted">Last updated 3 mins ago</small>
-      </div>
-    </div>
-  </div>
-</div>
+                    <div className="row row-cols-1 row-cols-md-3 g-4">
+                    <div className="col">
+                        <div className="card h-100">
+                        <img src="https://live.staticflickr.com/65535/51900488487_1b68a201c4_w.jpg" alt="new"/>
+                            <div className="card-body">
+                                <h5 className="card-title">BDS Quality Services</h5>
+                                <p className="card-text">Built using the Django framework and Python. BDS Quality Services is a company that provides quality assurance training and compliance.</p>
+                            </div>
+                            <div className="card-footer">
+                                <small className="text-muted">Copyright BDSQS</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card h-100">
+                            {/* <img src="..." className="card-img-top" alt="..."> */}
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                            </div>
+                            <div className="card-footer">
+                                <small className="text-muted">Last updated 3 mins ago</small>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card h-100">
+                            {/* <img src="..." className="card-img-top" alt="..."> */}
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                            </div>
+                            <div className="card-footer">
+                                <small className="text-muted">Last updated 3 mins ago</small>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                
-            
-           
-    </div>
+            </div>
+        </div>
         
         
     )
