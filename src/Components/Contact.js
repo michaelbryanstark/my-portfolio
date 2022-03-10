a8b804dc9bd19bd176fabf4ea9013b9916fae54a
michaelbryanstark <84143645+michaelbryanstark@users.noreply.github.com>
2022-03-10 11:27:54 -0500

    contact form works

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,11 +1,45 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Collapsible } from 'collapsible-react-component'
 import 'collapsible-react-component/dist/index.css'
 import ParticlesBg from 'particles-bg'
+import selfie from "../images/selfie.jpg"
+import emailjs from 'emailjs-com'
 
 
 const Contact = (props) => {
     const [open, setOpen] = React.useState(false)
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [message, setMessage] = useState('');
+    const [emailSent, setEmailSent] = useState(false);
+    const submit = () => {
+      if (name && email && message) {
+        const serviceId = 'service_2rh69zk';
+        const templateId = 'template_ighzcwc';
+        const userId = 'GWelQe7mDCutjawxE';
+        const templateParams = {
+            name,
+            email,
+            message
+        };
+
+        emailjs.send(serviceId, templateId, templateParams, userId)
+            .then(response => console.log(response))
+            .then(error => console.log(error));
+  
+          setName('');
+          setEmail('');
+          setMessage('');
+          setEmailSent(false);
+      } else {
+          alert('Please fill in all fields.');
+      }
+  }
+//   const isValidEmail = email => {
+//     const regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+//     return regex.test(String(email).toLowerCase());
+// };
+
     return (
         <>
         <div>
@@ -13,7 +47,7 @@ const Contact = (props) => {
             <h1 className="welcome-text">
                 Contact
             </h1>
-          
+            <img className="dan" src={selfie} alt="selfie"/>
         </div>
         <div className="intro">
         <>
@@ -27,7 +61,18 @@ const Contact = (props) => {
           {open ? 'Close' : 'Contact'}
         </button>
         <Collapsible open={open}>
-            Contact form
+        <div class="row mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Your Name</label>
+        <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Your Name" value={name} onChange={e => setName(e.target.value)} />
+        <label for="exampleFormControlInput1" class="form-label">Your Email</label>
+        <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" value={email} onChange={e => setEmail(e.target.value)} />
+        <label for="exampleFormControlTextarea1" class="form-label">Your message</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Your message" value={message} onChange={e => setMessage(e.target.value)}></textarea>
+        <button type='button'
+          className="btn btn-outline-secondary" onClick={submit}>Send Message</button>
+        <span className={emailSent ? 'visible' : null}>Thank you for your message, we will be in touch in no time!</span>
+        </div>
+            
         </Collapsible>
       </>
             </div>
