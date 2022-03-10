04cb73dd517d271ff139a063c422a4eb9aa7dff3
michaelbryanstark <84143645+michaelbryanstark@users.noreply.github.com>
2022-03-10 11:40:42 -0500

    tweaked bio

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -32,7 +32,7 @@ const Welcome = (props) => {
           {open ? 'Close' : 'Who am I?'}
         </button>
         <Collapsible open={open}>
-        I am a Software Engineer with a background in Physics and Operations Management. Problem solving has always been my focus and drive to improve how things operate and introduce logical systems. Throughout my career managing an art auction house, I have learned how to oversee multiple tasks and create new solutions to streamline workflows and get the job done. Using my skills in Javascript, Python, HTML, and CSS, I am able to create simpler, more coherent software to meet the needs of an ever-changing environment.
+        I am a Software Engineer with a background in Physics and Operations Management. Problem solving has always been my focus and drive to improve how things operate and introduce logical systems. Throughout my career in Web Development, I have learned how to oversee multiple tasks and create new solutions to streamline workflows and get the job done. Using my skills in Javascript, Python, HTML, and CSS, I am able to create simpler, more coherent software to meet the needs of an ever-changing environment.
         </Collapsible>
       </>
             </div>
