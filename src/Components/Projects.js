d36cdfeec692c3e279e9d53fed91da673360cf30
michaelbryanstark <84143645+michaelbryanstark@users.noreply.github.com>
2022-05-11 00:43:59 -0400

    slight changes to boxes and radius, removed card body from projects

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -29,7 +29,7 @@ function Projects() {
           {open ? 'Close' : 'Apps and Websites'}
         </button>
         <Collapsible open={open}>
-                <div className="card-header">
+                
                     <div className="row row-cols-1 row-cols-md-3 g-4">
                     <div className="col">
                         <div className="card text-center h-100">
@@ -77,7 +77,6 @@ function Projects() {
                         </div>
                     </div>
                 </div>
-            </div>
             </Collapsible>
         </>
         </div>
