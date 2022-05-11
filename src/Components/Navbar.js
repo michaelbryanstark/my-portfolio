917c3e9ba5dfec0026506b674b1f8ec0ba71e9ff
michaelbryanstark <84143645+michaelbryanstark@users.noreply.github.com>
2022-05-11 01:14:20 -0400

    made navbar light

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,7 +3,7 @@ import { Link, NavLink } from 'react-router-dom';
 import { SocialIcon } from 'react-social-icons';
 
 const navbar = () => (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <Link className="navbar-brand" to='/'>Michael Bryan Stark</Link>
         <button 
             className="navbar-toggler"
